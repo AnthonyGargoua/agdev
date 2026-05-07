8301ef67988a93343858b79ce3f077f954004638
Anthony GARGOUA <56581894+AnthonyGargoua@users.noreply.github.com>
2026-05-07 12:36:14 +0200

    script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,12 @@ if (intro) {
 // --- ANIMATIONS GLOBALES ---
 function launchPageAnimations() {
     // Apparition des textes
-    gsap.to(".fade", { opacity: 1, y: 0, duration: 0.8, stagger: 0.2, ease: "power2.out" });
+    gsap.to(".progress-line span", {
+    scaleX: 1,
+    duration: 1.5,
+    ease: "power2.out",
+    stagger: 0.1
+});
     
     // Animation des catégories de compétences
     if (document.querySelector(".skill-category")) {
